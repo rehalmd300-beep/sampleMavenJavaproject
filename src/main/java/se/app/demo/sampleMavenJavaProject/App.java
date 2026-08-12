07c50b56c677066c382bbafd55f40f1ce481f3d8
Mohammed Rehal <admin@gmail.com>
2026-08-12 15:59:59 +0530

    sample maven java

---
+++ src/main/java/se/app/demo/sampleMavenJavaProject/App.java
@@ -0,0 +1,13 @@
+package se.app.demo.sampleMavenJavaProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
